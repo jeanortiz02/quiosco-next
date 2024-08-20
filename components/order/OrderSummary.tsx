572dfe4a27ce3445d96d6b54bf04f2d74eb84e0a
Jean Carlos Ortiz <105516848+jeanortiz02@users.noreply.github.com>
2024-08-20 16:03:06 -0400

    7 videos

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -2,17 +2,23 @@
 
 import { useStore } from "@/src/utils/store"
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils/Index";
 
 export default function OrderSummary() {
 
   const order = useStore((state) => state.order);
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) ,[order])
 
+  const handleCreateOrder = () => {
+    console.log('Desde.........')
+  }
   
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
         <h1 className="font-black text-4xl text-center">Mi pedido</h1>
 
-        {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
+        {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
           <div className="mt-5">
             {order.map(item => (
               <ProductDetails 
@@ -20,6 +26,22 @@ export default function OrderSummary() {
                 item={item}
               />
             ))}
+
+            <p className="text-2xl mt-20 text-center">
+              Total a pagar {' '}
+                <span className="font-bold">{formatCurrency(total)}</span>
+            </p>
+            <form 
+              className="w-full mt-10 space-y-5"
+              action={handleCreateOrder}
+            >
+
+              <input 
+                type="submit"
+                className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                value='Confirmar pedido'
+              />
+            </form>
           </div>
         )}
     </aside>
--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -1,32 +1,56 @@
 import { OrderItem } from "@/src/types";
 import { formatCurrency } from "@/src/utils/Index";
+import { useStore } from "@/src/utils/store";
 import { MinusIcon, PlusIcon, XCircleIcon } from '@heroicons/react/24/outline'
+import { useMemo } from "react";
 
 type ProductDetailsProps = {
   item: OrderItem;
 };
 
+const MIN_ITEM = 1
+const MAX_ITEM = 5
+
 export default function ProductDetails({ item }: ProductDetailsProps) {
-  console.log(item);
+
+  const increaseQuantity = useStore((state) => state.increaseQuantity);
+  const decreaseincreaseQuantity = useStore((state) => state.decreaseincreaseQuantity);
+  const removeItem = useStore((state) => state.removeItem);
+  const desableDrecreaseButton = useMemo(() => item.quantity === MIN_ITEM, [item])
+  const desableIncreaseButton = useMemo(() => item.quantity === MAX_ITEM, [item])
+
   return (
     <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
       <div className="space-y-4">
         <div className="flex justify-between items-start">
           <p className="text-xl font-bold">{item.name} </p>
 
-          <button type="button" onClick={() => {}}>
+          <button 
+            type="button" 
+            onClick={() => removeItem(item.id)}
+          >
             <XCircleIcon className="text-red-600 h-8 w-8" />
           </button>
         </div>
         <p className="text-2xl text-amber-500 font-black">{formatCurrency(item.price)}</p>
         <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
-          <button type="button" onClick={() => {}}>
+          <button 
+            type="button" 
+            onClick={() => decreaseincreaseQuantity(item.id)}
+            disabled={desableDrecreaseButton}
+            className="disabled:opacity-20"
+          >
             <MinusIcon className="h-6 w-6" />
           </button>
 
           <p className="text-lg font-black ">{item.quantity}</p>
 
-          <button type="button" onClick={() => {}}>
+          <button 
+            type="button" 
+            onClick={() => increaseQuantity(item.id)}
+            disabled={desableIncreaseButton}
+            className="disabled:opacity-20"
+          >
             <PlusIcon className="h-6 w-6" />
           </button>
         </div>
