990a5e4367dd96c115abf4ba6acb64bd5961dc98
Jean Carlos Ortiz <105516848+jeanortiz02@users.noreply.github.com>
2024-08-22 16:05:05 -0400

    5 videos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,32 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+
+async function getProducts () {
+
+  const products = await prisma.product.findMany({
+    include: {
+      category: true
+    }
+  })
+
+  return products;
+ 
+}
+
+export type ProductsWithCategory = Awaited<ReturnType <typeof getProducts>>
 
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+
+  const products = await getProducts();
   return (
     
     <>
-
       <Heading>Administrar Productos</Heading>
+      <ProductTable
+        products={products}
+      />
     </>
   )
 }
--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,4 +1,6 @@
+import { completeOrder } from "@/actions/complete-order-action"
 import { OrderWithProducts } from "@/src/types"
+import { formatCurrency } from "@/src/utils/Index"
 
 
 type OrderCardProps = {
@@ -6,6 +8,7 @@ type OrderCardProps = {
 }
 
 export default function OrderCard({ order } : OrderCardProps) {
+
 
     return (
         <section
@@ -15,14 +18,35 @@ export default function OrderCard({ order } : OrderCardProps) {
             <p className='text-2xl font-medium text-gray-900'>Cliente: {order.name}</p>
             <p className='text-lg font-medium text-gray-900'>Productos Ordenados:</p>
             <dl className="mt-6 space-y-4">
-        
+                {order.orderProducts.map( product => (
+                    <div
+                        key={product.id}
+                        className="flex items-center gap-2 border-t border-gray-200 pt-2"
+                    >
+
+                        <dt className="flex items-center text-sm text-gray-600">
+                            <span className="font-black">({product.quantity}) {''}</span>
+                        </dt>
+
+                        <dd className="text-sm font-medium text-gray-900">{product.product.name}</dd>
+
+                    </div>
+                ))}
                 <div className="flex items-center justify-between border-t border-gray-200 pt-4">
                     <dt className="text-base font-medium text-gray-900">Total a Pagar:</dt>
-                    <dd className="text-base font-medium text-gray-900">{}</dd>
+                    <dd className="text-base font-medium text-gray-900">{formatCurrency(order.total)}</dd>
                 </div>
             </dl>
 
-            <form>
+            <form
+                action={completeOrder}
+            >
+
+                <input
+                    type='hidden'
+                    value={order.id}
+                    name='order_id'
+                />
                 <input
                     type="submit"
                     className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
